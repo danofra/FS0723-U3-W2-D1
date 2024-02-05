d8523885b86dab813f5537d763b1a1e95d84525d
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-02-05 16:34:49 +0100

    first commit

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,94 @@
+import { Component } from 'react'
+import { Button, Form } from 'react-bootstrap'
+
+class AddComment extends Component {
+  state = {
+    comment: {
+      comment: '',
+      rate: 1,
+      elementId: this.props.asin,
+    },
+  }
+
+  sendComment = async (e) => {
+    e.preventDefault()
+    try {
+      let response = await fetch(
+        'https://striveschool-api.herokuapp.com/api/comments',
+        {
+          method: 'POST',
+          body: JSON.stringify(this.state.comment),
+          headers: {
+            'Content-type': 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWMwZjE1YWUwODVmYTAwMTk2MzFhODUiLCJpYXQiOjE3MDcxNDM1MTQsImV4cCI6MTcwODM1MzExNH0.B6krWrT9m1tHOfbkDR3DXPq6U6NBTw9mR4YRysEf1lo',
+          },
+        }
+      )
+      if (response.ok) {
+        alert('Recensione inviata!')
+        this.setState({
+          comment: {
+            comment: '',
+            rate: 1,
+            elementId: this.props.asin,
+          },
+        })
+      } else {
+        throw new Error('Qualcosa è andato storto')
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  render() {
+    return (
+      <div className="my-3">
+        <Form onSubmit={this.sendComment}>
+          <Form.Group className="mb-2">
+            <Form.Label>Recensione</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Inserisci qui il testo"
+              value={this.state.comment.comment}
+              onChange={(e) =>
+                this.setState({
+                  comment: {
+                    ...this.state.comment,
+                    comment: e.target.value,
+                  },
+                })
+              }
+            />
+          </Form.Group>
+          <Form.Group className="mb-2">
+            <Form.Label>Valutazione</Form.Label>
+            <Form.Control
+              as="select"
+              value={this.state.comment.rate}
+              onChange={(e) =>
+                this.setState({
+                  comment: {
+                    ...this.state.comment,
+                    rate: e.target.value,
+                  },
+                })
+              }
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </Form.Control>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Invia
+          </Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default AddComment
---
+++ src/components/BookList.jsx
@@ -0,0 +1,46 @@
+import { Component } from "react";
+import SingleBook from "./SingleBook";
+import { Col, Form, Row } from "react-bootstrap";
+import CommentArea from "./CommentArea";
+
+class BookList extends Component {
+  state = {
+    searchQuery: "",
+    bookId: null,
+  };
+
+  render() {
+    return (
+      <>
+        <Form.Group>
+          <Form.Control
+            type="search"
+            placeholder="Cerca un libro"
+            value={this.state.searchQuery}
+            onChange={(e) => this.setState({ searchQuery: e.target.value })}
+          />
+        </Form.Group>
+        <Row className="justify-content-centermt-5">
+          <Col className="col-8 text-center">
+            <Row className="justify-content-center">
+              {this.props.books
+                .filter((b) =>
+                  b.title.toLowerCase().includes(this.state.searchQuery)
+                )
+                .map((b) => (
+                  <Col key={b.asin} className="col-md-3">
+                    <SingleBook book={b} />
+                  </Col>
+                ))}
+            </Row>
+          </Col>
+          <Col className="col-4 text-center sticky-top mt-5 h-50">
+            <CommentArea asin={this.state.bookId} />
+          </Col>
+        </Row>
+      </>
+    );
+  }
+}
+
+export default BookList;
